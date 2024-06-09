f7ae9f109bdf1ceb73d80b34a262d46ce51e8e14
Aler1x <95098796+Aler1x@users.noreply.github.com>
2024-06-09 21:13:32 +0300

    feat: add loading state (now it not work on 8GB of ram)

--- components/MyAuctions.tsx
+++ components/MyAuctions.tsx
@@ -9,23 +9,25 @@ const MyAuctions = () => {
   const { data } = useSession();
 
   const [auctions, setAuctions] = useState([] as Auction[]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (!data?.user?.email) return;
+    setLoading(true);
     clientFetchJson<Auction[]>(`auction?createdBy=${data?.user?.email}`).then((auctions) =>
       setAuctions(auctions)
-    );
+    ).finally(() => setLoading(false));
   }, [data?.user?.email]);
 
   return (
     <>
-      {auctions.length > 0 ? (
-        <Auctions auctions={auctions} myAuctions={true} className="grid xl:grid-cols-2" />
-      ) : (
+      {loading ? (
         <div className="grid xl:grid-cols-2 gap-4">
           <Skeleton className="h-[200px] w-[100%]" />
           <Skeleton className="h-[200px] w-[100%]" />
         </div>
+      ) : (
+        <Auctions auctions={auctions} myAuctions={true} className="grid xl:grid-cols-2" />
       )}
     </>
   )
